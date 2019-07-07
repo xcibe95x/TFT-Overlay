23e7b73b0b94afe212c000efea00adee1a035093
xcibe95x <xcibe95x@gmail.com>
2019-07-07 16:38:03 +0200

    Assets Update

--- Form1.cs
+++ Form1.cs
@@ -882,7 +882,7 @@ namespace TFT_Overlay
                 var picture = new PictureBox
                 {
                     Name = "pictureBox",
-                    Size = new Size(42, 42),
+                    Size = new Size(52, 52),
                     Location = new Point(100, 100),
                     BackgroundImage = (Image)(rm.GetObject(champName)),
                     BackgroundImageLayout = ImageLayout.Stretch,
