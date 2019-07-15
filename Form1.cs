96019bf8807cc6fd7bb8cf345a7e22a4de333b43
xcibe95x <xcibe95x@gmail.com>
2019-07-15 17:55:51 +0200

    Updated to version 2.2.1

--- Form1.cs
+++ Form1.cs
@@ -99,7 +99,7 @@ namespace TFT_Overlay
             File.WriteAllBytes(Path.GetTempPath() + "Normal.cur", Properties.Resources.Normal);
             File.WriteAllBytes(Path.GetTempPath() + "Pointer.cur", Properties.Resources.Pointer);
 
-            var localVersion = new Version("2.2");
+            var localVersion = new Version("2.2.1");
 
             // Program Title
             Title.Text = "TFT Overlay | by @xcibe95x";
